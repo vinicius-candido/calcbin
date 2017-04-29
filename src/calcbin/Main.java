def03b41e7009813d8afe73394e659c0a9866dc1
Vinicius Candido de Carvalho <viniciuscandido.ti@gmail.com>
2017-04-29 12:01:13 -0300

    melhoria

--- src/calcbin/Main.java
+++ src/calcbin/Main.java
@@ -12,28 +12,49 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Scanner n = new Scanner(System.in);
-		System.out.println("1 - Converter base 10 para base X\n");
-		System.out.println("2 - Converter base x para base 10\n");
-		int op = n.nextInt();
-		if(op==1){
-			System.out.println("Informe um numero binario:");
-			String x;
-			x = n.next();
+		int op, in, base;
+		String x;
+		
+		do{
+			System.out.println("Conversao de base - Selecione uma das opcoes abaixo:");
+			System.out.print("1 - Converter base 10 para base X\n");
+			System.out.print("2 - Converter base x para base 10\n");
+			System.out.print("0 - Para sair\n");
+			op = n.nextInt();
 			
-			System.out.println("Informe a base para converter (10-decimal, 8-octal, 16-hexadecimal)");
-			int bas = n.nextInt();
-			binarioParaX(x, bas);
-		}
+			if (op==1) {
+				System.out.println("Informe um numero decimal:");
+				in = n.nextInt();
+				System.out.println("Informe a base para converter (2-binario, 3-base 3, 4-base 4, 8-octal, 16-hexadecimal)");
+				base = n.nextInt();
+				System.out.print("Resultado de "+ in +" em base "+ base +": ");
+				converteRecursivo(in, base);
+				System.out.println("\n");
+			}
+			if(op==2){
+				System.out.println("Informe um numero binario:");	
+				x = n.next();
+				System.out.println("Informe a base para converter (3-base 3, 4-base 4, 8-octal, 10-decimal, 16-hexadecimal)");
+				base = n.nextInt();
+				//binarioParaX(x, bas);
+			}
+		}while(op != 0);
 	}
-
-	private static void binarioParaX(String bin, int base) {
-		switch (base) {
-		case 10:
-			System.out.println("Resultado: " +Integer.parseInt(bin, 2));
-			break;
-		default:
-			break;
+	
+	private static void converteRecursivo(int numero, int base){
+		if(numero>0){
+			converteRecursivo(numero/base, base);
+			System.out.print(numero%base);
 		}
 	}
 	
+//	private static void binarioParaX(String bin, int base) {
+//		switch (base) {
+//		case 10:
+//			System.out.println("Resultado: " +Integer.parseInt(bin, 2));
+//			break;
+//		default:
+//			break;
+//		}
+//	}
 }
