afb425f579c82b25b37582359c54c93b257c1106
Vinicius Candido de Carvalho <viniciuscandido.ti@gmail.com>
2017-04-29 01:40:53 -0300

    mudei tudo

--- src/calcbin/Main.java
+++ src/calcbin/Main.java
@@ -6,11 +6,34 @@
 
 package calcbin;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	
 	public static void main(String[] args) {
-		StringBuffer bf;
-		
+		Scanner n = new Scanner(System.in);
+		System.out.println("1 - Converter base 10 para base X\n");
+		System.out.println("2 - Converter base x para base 10\n");
+		int op = n.nextInt();
+		if(op==1){
+			System.out.println("Informe um numero binario:");
+			String x;
+			x = n.next();
+			
+			System.out.println("Informe a base para converter (10-decimal, 8-octal, 16-hexadecimal)");
+			int bas = n.nextInt();
+			binarioParaX(x, bas);
+		}
 	}
 
+	private static void binarioParaX(String bin, int base) {
+		switch (base) {
+		case 10:
+			System.out.println("Resultado: " +Integer.parseInt(bin, 2));
+			break;
+		default:
+			break;
+		}
+	}
+	
 }
